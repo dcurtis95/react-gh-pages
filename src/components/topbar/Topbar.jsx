2f9f87f608a7dde5b157abaa2c0fc1a926b31ef5
Daniel Curtis <danielcurtis95@gmail.com>
2021-09-20 16:21:03 +0100

    menu added

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,31 +1,32 @@
 import "./topbar.scss"
 import "./Daniel Curtis.png"
-// import {Code,Mail} from '@material-ui';
-// import CodeIcon from '@mui/icons-material/Code';
-// import { CodeIcon, MailIcon } from '@mui/icons-material';
-// import { CodeIcon } from '@mui/icons-material/Code';
-// import { MailIcon } from '@mui/icons-material/Mail';
-// import { CodeIcon } from '@mui/icons-material/Code';
-// import { MailIcon } from '@mui/icons-material/Mail';
+import MailIcon from '@mui/icons-material/Mail';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className="logo">dcurtis.</a>
+          <a href="#intro" className="logo">
+            dcurtis.
+            </a>
           {/* <a href="#intro" className="logo"><img alt="Daniel Curtis" src={require("./Daniel Curtis.png").default} height={55} width={70}></img></a> */}
           <div className="itemContainer">
-            {/* <CodeIcon className="icon" /> */}
-              <span></span>
+            {/* <MailIcon className="icon" />
+            <span>danielcurtis95@gmail.com</span> */}
           </div>
           <div className="itemContainer">
-            {/* <MailIcon className="icon" /> */}
-            {/* <span>danielcurtis95@gmail.com</span> */}
+            <span></span>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
