caf00a7eff198484c30f3924eaa936fc590527a7
Daniel Curtis <danielcurtis95@gmail.com>
2021-09-20 13:45:15 +0100

    preparing for use of material ui icons, also smoothing to scrool effects

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,31 @@
 import "./topbar.scss"
+import "./Daniel Curtis.png"
+// import {Code,Mail} from '@material-ui';
+// import CodeIcon from '@mui/icons-material/Code';
+// import { CodeIcon, MailIcon } from '@mui/icons-material';
+// import { CodeIcon } from '@mui/icons-material/Code';
+// import { MailIcon } from '@mui/icons-material/Mail';
+// import { CodeIcon } from '@mui/icons-material/Code';
+// import { MailIcon } from '@mui/icons-material/Mail';
 
 export default function Topbar() {
   return (
     <div className="topbar">
-
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">dcurtis.</a>
+          {/* <a href="#intro" className="logo"><img alt="Daniel Curtis" src={require("./Daniel Curtis.png").default} height={55} width={70}></img></a> */}
+          <div className="itemContainer">
+            {/* <CodeIcon className="icon" /> */}
+              <span></span>
+          </div>
+          <div className="itemContainer">
+            {/* <MailIcon className="icon" /> */}
+            {/* <span>danielcurtis95@gmail.com</span> */}
+          </div>
+        </div>
+        <div className="right"></div>
+      </div>
     </div>
   )
 }
