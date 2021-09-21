5c85d20c70211452143169127105339b3e2c19f8
Daniel Curtis <danielcurtis95@gmail.com>
2021-09-21 20:00:06 +0100

    1st draft of portfolio page created

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,27 +1,70 @@
-import "./portfolio.scss"
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
+import "./portfolio.scss";
+import { featuredPortfolio, webPortfolio, mobilePortfolio, rubyPortfolio, reactPortfolio } from "../../data";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile / React",
+    },
+    {
+      id: "ruby",
+      title: "Ruby on Rails",
+    },
+  ];
+
+  useEffect(()=>{
+    switch(selected){
+      case "web":
+      setData(webPortfolio);
+      break;
+      case "mobile":
+      setData(mobilePortfolio);
+      break;
+      case "ruby":
+      setData(rubyPortfolio);
+      break;
+      case "react":
+      setData(reactPortfolio);
+      break;
+      default:
+      setData(webPortfolio);
+    }
+
+  },[selected])
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">Featured</li>
-        <li>Web App</li>
-        <li>Mobile App</li>
+        {list.map((item) => (
+          <PortfolioList
+          title={item.title}
+          active={selected === item.id}
+          setSelected={setSelected}
+          id={item.id}
+          />
+        ))}
       </ul>
       <div className="container">
+        {data.map(d=>(
+
+
         <div className="item">
-          <img src="assets/BeNatural.png" alt="" />
-            <h3>Daniel Curtis Portfolio</h3>
+          <a href={d.url}>
+              <img src={d.img} alt={d.title} />
+          </a>
+            <h3>{d.title}</h3>
         </div>
-        <div className="item">
-          <img src="assets/BeNatural.png" alt="" />
-          <h3>Daniel Curtis Portfolio</h3>
-        </div>
-        <div className="item">
-          <img src="assets/BeNatural.png" alt="" />
-          <h3>Daniel Curtis Portfolio</h3>
-        </div>
+        ))}
       </div>
     </div>
   )
---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: "BeNatural",
+    img: "assets/BeNatural.png",
+    url: "http://be-natural-project.herokuapp.com/"
+  },
+  {
+    id: 2,
+    title: "LaCasserole",
+    img: "assets/LaCasserole.png",
+    url: "https://la-casserole-new.herokuapp.com/chefs"
+  },
+  {
+    id: 3,
+    title: "Portfolio",
+    img: "assets/Portfolio.png",
+    url: "https://www.daniel-curtis.co.uk/"
+  },
+];
+
+export const webPortfolio = [
+  {
+    id: 1,
+    title: "BeNatural",
+    img: "assets/BeNatural.png",
+    url: "http://be-natural-project.herokuapp.com/"
+  },
+  {
+    id: 2,
+    title: "LaCasserole",
+    img: "assets/LaCasserole.png",
+    url: "https://la-casserole-new.herokuapp.com/chefs"
+  },
+  {
+    id: 3,
+    title: "Portfolio",
+    img: "assets/Portfolio.png",
+    url: "https://www.daniel-curtis.co.uk/"
+  },
+];
+
+export const mobilePortfolio = [
+  {
+    id: 1,
+    title: "Portfolio",
+    img: "assets/Portfolio.png",
+    url: "https://www.daniel-curtis.co.uk/"
+  },
+];
+
+export const rubyPortfolio = [
+  {
+    id: 1,
+    title: "BeNatural",
+    img: "assets/BeNatural.png",
+    url: "http://be-natural-project.herokuapp.com/"
+  },
+  {
+    id: 2,
+    title: "LaCasserole",
+    img: "assets/LaCasserole.png",
+    url: "https://la-casserole-new.herokuapp.com/chefs"
+  },
+];
+
+export const reactPortfolio = [
+  {
+    id: 1,
+    title: "Portfolio",
+    img: "assets/Portfolio.png",
+    url: "https://www.daniel-curtis.co.uk/"
+  },
+];
