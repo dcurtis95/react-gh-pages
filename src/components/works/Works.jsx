a7538d75804ea2c0656202634e962eaaa73fd361
Daniel Curtis <danielcurtis95@gmail.com>
2021-09-22 11:39:34 +0100

    works page created

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,9 +1,65 @@
+import { useState } from "react";
 import "./works.scss"
 
 export default function Works() {
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const data = [
+    {
+      id: 1,
+      title: "BeNatural",
+      img: "assets/BeNatural.png",
+      url: "http://be-natural-project.herokuapp.com/",
+      icon: "assets/globe.png",
+      desc: "A web app designed and built as part of a small team during Le Wagon, allowing users to order organic farm produce from local farms."
+    },
+    {
+      id: 2,
+      title: "LaCasserole",
+      img: "assets/LaCasserole.png",
+      url: "https://la-casserole-new.herokuapp.com/chefs",
+      icon: "assets/globe.png",
+      desc: "A web app designed and built as part of a small team during Le Wagon, allowing users to hire personal chefs to cook for them."
+    },
+    {
+      id: 3,
+      title: "Portfolio",
+      img: "assets/Portfolio.png",
+      url: "https://www.daniel-curtis.co.uk/",
+      icon: "assets/mobile.png",
+      desc: "A responsive react project designed and built by me, my first react project."
+    },
+  ];
+
+  const handleClick = (way)=>{
+    way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2) :
+    setCurrentSlide(currentSlide<data.length -1 ?currentSlide+1 : 0);
+  }
   return (
     <div className="works" id="works">
-
+      <div className="slider" style={{transform: `translateX(-${currentSlide *100}vw)`}}>
+        {data.map(d=>(
+         <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src={d.icon} alt="Web App" />
+                </div>
+                <h2>{d.title}</h2>
+                <p>
+                  {d.desc}
+                </p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right">
+                <img src={d.img} alt="Be Natural" />
+            </div>
+          </div>
+        </div>))};
+      </div>
+      <img src="assets/arrow.png" className="arrow left" alt="arrow" onClick={()=>handleClick("left")}/>
+      <img src="assets/arrow.png" className="arrow right" alt="arrow" onClick={() => handleClick("right")}/>
     </div>
   )
 }
