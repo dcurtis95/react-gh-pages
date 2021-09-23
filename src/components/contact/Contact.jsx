dd170604d63a67208025e48cc530171fec73dd43
Daniel Curtis <danielcurtis95@gmail.com>
2021-09-23 17:37:07 +0100

    added email support to contact page (users can send realy emails)

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,23 +1,36 @@
 import { useState } from "react";
+import React, { useRef } from "react";
+import emailjs from 'emailjs-com';
 import "./contact.scss";
 
 export default function Contact() {
   const [contactStyle, setContactStyle] = useState(false);
   const [message, setMessage] = useState(false);
+  const form = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setMessage(true);
+
+    emailjs.sendForm('service_rlplb9b', 'template_drioq9b', form.current, 'user_UTUIbQjFoKO7fyoi9L1tE')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+      e.target.reset()
+      setMessage(true);
   };
   return (
     <div className={"contact " + (contactStyle && "active")} id="contact">
       <div className="item">
         <div className="right">
           <h2 onClick={() => setContactStyle(!contactStyle)}>Contact.</h2>
-          <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Email" />
-            <textarea placeholder="Message"></textarea>
-            <button type="submit">Send</button>
+          <form ref={form} onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" name="name" />
+            <input type="text" placeholder="Subject" name="subject" />
+            <input type="text" placeholder="Email" name="email" />
+            <textarea placeholder="Message" name="message"></textarea>
+            <button type="submit" value="Send">Send</button>
             {message && <span>Thanks, I'll reply ASAP :)</span>}
           </form>
         </div>
